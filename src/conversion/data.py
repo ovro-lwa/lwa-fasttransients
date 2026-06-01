b460d64639090782272116f6ac069b78fe36a847
Casey Law <caseyjlaw@gmail.com>
2026-06-01 10:16:05 -0700

    modify org of data reader

--- src/conversion/data.py
+++ src/conversion/data.py
@@ -1,57 +1,40 @@
 """
-HDF5 beam-file helpers from lwa-project/commissioning DRX/HDF5.
+HDF5 beam-file helpers (lwa-project commissioning DRX/HDF5).
 
-Re-exports :mod:`conversion._data` (vendored ``_data.py``). Other code should use::
+Public API for the rest of the pipeline::
 
     from conversion import data as hdfData
 
-which is the same API as ``from conversion._data import *``.
+Implementation is in :mod:`conversion.hdf5_beam_data` (file ``hdf5_beam_data.py``).
 """
 
+import importlib.util
 import os
-import time
-from urllib import request as urlrequest
 
-MODULE_URL = (
-    "https://raw.githubusercontent.com/lwa-project/commissioning/master/DRX/HDF5/data.py"
-)
-MODULE_PATH = os.path.dirname(os.path.abspath(__file__))
-_DATA_FILE = os.path.join(MODULE_PATH, "_data.py")
-_ETAG_FILE = os.path.join(MODULE_PATH, "_data.etag")
-MAX_AGE_SEC = 86400
+_MODULE_DIR = os.path.dirname(os.path.abspath(__file__))
+_HDF5_BEAM_DATA_FILE = os.path.join(_MODULE_DIR, "hdf5_beam_data.py")
 
 
-def _maybe_refresh_data_file():
-    """Download commissioning data.py into ``_data.py`` if missing or stale."""
-    age = 1e6
-    etag = ""
-    if os.path.isfile(_DATA_FILE):
-        age = time.time() - os.path.getmtime(_DATA_FILE)
-        if os.path.isfile(_ETAG_FILE):
-            with open(_ETAG_FILE, "r", encoding="utf-8") as fh:
-                etag = fh.read()
-    if age <= MAX_AGE_SEC:
-        return
-    request = urlrequest.Request(MODULE_URL)
-    with urlrequest.urlopen(request, timeout=60) as response:
-        new_etag = response.headers.get("etag", "")
-        body = response.read()
-    if new_etag == etag and os.path.isfile(_DATA_FILE):
-        return
-    with open(_DATA_FILE, "wb") as fh:
-        fh.write(body)
-    with open(_ETAG_FILE, "w", encoding="utf-8") as fh:
-        fh.write(new_etag)
-
-
-_maybe_refresh_data_file()
-if not os.path.isfile(_DATA_FILE):
-    raise FileNotFoundError(
-        "Missing {0}; run once on a networked host or copy from "
-        "https://github.com/lwa-project/commissioning/tree/master/DRX/HDF5".format(
-            _DATA_FILE
+def _load_hdf5_beam_data():
+    if not os.path.isfile(_HDF5_BEAM_DATA_FILE):
+        raise FileNotFoundError(
+            "Missing {0}. Run 'git pull' in lwa-fasttransients or copy from "
+            "https://github.com/lwa-project/commissioning/tree/master/DRX/HDF5/data.py".format(
+                _HDF5_BEAM_DATA_FILE
+            )
         )
+    spec = importlib.util.spec_from_file_location(
+        "conversion.hdf5_beam_data",
+        _HDF5_BEAM_DATA_FILE,
     )
+    if spec is None or spec.loader is None:
+        raise ImportError("Could not load {0}".format(_HDF5_BEAM_DATA_FILE))
+    module = importlib.util.module_from_spec(spec)
+    spec.loader.exec_module(module)
+    return module
 
-from ._data import *
-from ._data import __all__
+
+_hdf5 = _load_hdf5_beam_data()
+__all__ = list(getattr(_hdf5, "__all__", []))
+for _name in __all__:
+    globals()[_name] = getattr(_hdf5, _name)
